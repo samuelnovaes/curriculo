e1d4625c73765d1078c0873a2d3df24542239fd2
Samuel Novaes <samuel.novaes.96@gmail.com>
2024-09-01 15:05:20 -0300

    perform toggle icon

--- scripts/global.js
+++ scripts/global.js
@@ -1,13 +1,19 @@
 const menuToggle = document.getElementById('menu-toggle');
 const menu = document.getElementById('menu');
 
+// Função para alterar o ícone do menu.
+const toggleIcon = () => {
+  menuToggle.style.backgroundImage = menu.style.display == 'block' ? 'url(img/icons/close.svg)' : 'url(img/icons/menu.svg)';
+};
+
 // Exibe ou oculta o menu em dispositivos móveis.
 menuToggle.addEventListener('click', () => {
   menu.style.display = menu.style.display == 'block' ? 'none' : 'block';
-  menuToggle.style.backgroundImage = menu.style.display == 'block' ? 'url(img/icons/close.svg)' : 'url(img/icons/menu.svg)';
+  toggleIcon();
 });
 
 // Ao redimensionar a janela deve exibir ou ocultar o menu.
 window.addEventListener('resize', () => {
   menu.style.display = window.innerWidth >= 1000 ? 'block' : 'none';
+  toggleIcon();
 });
